f5e254e03659e6d041b47e80cf5f079ff16117ec
Javier Diaz <jdiaz@inelde.com>
2015-06-23 17:02:10 +0200

    controlador destacado

--- src/main/java/curso/tecnocom/gestor/DestacadoController.java
+++ src/main/java/curso/tecnocom/gestor/DestacadoController.java
@@ -5,13 +5,14 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import curso.tecnocom.gestor.datos.Destacado;
 import curso.tecnocom.gestor.delegates.DestacadosDelegate;
-
+@Controller
 public class DestacadoController {
 
 	private static final Logger logger = LoggerFactory
