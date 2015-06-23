a31c439eceaa2fd18a11db64c204c03f329fa839
alumno <alumno@londres12.formacion.local>
2015-06-23 16:03:43 +0200

    noticias

---
+++ src/main/java/curso/tecnocom/gestor/services/NoticiasService.java
@@ -0,0 +1,56 @@
+package curso.tecnocom.gestor.services;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import curso.tecnocom.gestor.daos.NoticiasDaos;
+
+
+public class NoticiasService {
+
+	/***** Propiedades de la clase *****/
+	@Autowired
+	private NoticiasDao noticiasDao;
+	
+	public List<?>dameDatos(Class<?>clase) throws Exception
+	{
+		List<?>salida=null;
+		Method metodo=getNoticiasDao().getClass().getMethod("dameDatos",Class.class);
+		salida=(List<?>) metodo.invoke(getNoticiasDao(), clase);
+				
+		return salida;
+	}
+	
+	public NoticiasDao getNoticiasDao(){
+		return noticiasDao;
+	}
+	
+	public void setNoticiasDao(NoticiasDao noticiasDao){		
+		this.noticiasDao=noticiasDao;
+	}
+	
+	public void borrar(int id, Class<?>clase) throws Exception{
+		Method metodo=getNoticiasDao().getClass().getMethod("borrar",int.class,Class.class);
+		metodo.invoke(getNoticiasDao(), id,clase);
+	}
+	
+	public Object dameObjeto(int id, Class<?> clase) throws Exception {
+		Method metodo=getNoticiasDao().getClass().getMethod("dameObjeto", int.class,Class.class);
+		 return metodo.invoke(getNoticiasDao(), id,clase);
+	}
+	
+
+	public void grabaObjeto(Object objeto) throws Exception{
+		Method metodo=getNoticiasDao().getClass().getMethod("grabaObjeto", Object.class);
+		metodo.invoke(getNoticiasDao(), objeto);
+		
+	}
+
+
+	
+	
+	
+}
