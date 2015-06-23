944e2ad3de6f42abab638e7806659f8dd1d7fe83
profesor <profesor@londres1.formacion.local>
2015-06-23 17:14:59 +0200

    Controller Principal

--- src/main/java/curso/tecnocom/gestor/PrincipalController.java
+++ src/main/java/curso/tecnocom/gestor/PrincipalController.java
@@ -9,75 +9,59 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import curso.tecnocom.gestor.datos.Principale;
 import curso.tecnocom.gestor.delegates.PrincipalDelegate;
 
-
-
-
-
-
-
 @Controller
 public class PrincipalController {
 
-private static final Logger logger = LoggerFactory.getLogger(PrincipalController.class);
-	
+	private static final Logger logger = LoggerFactory
+			.getLogger(PrincipalController.class);
+
 	// cambio del profesor
 
 	@Autowired
 	private PrincipalDelegate principalDelegate;
 
-	
-	
-	
-	@RequestMapping("principal.html")	
-	public ModelAndView principales()
-	{
+	@RequestMapping("principal.html")
+	public ModelAndView principales() {
 		List<Principale> principales;
 		try {
-			principales=(List<Principale>)getPrincipalDelegate().dameDatos(Principale.class);
-			Set<Principale> principalesOrdenados= new TreeSet<Principale>();
+			principales = (List<Principale>) getPrincipalDelegate().dameDatos(
+					Principale.class);
+			Set<Principale> principalesOrdenados = new TreeSet<Principale>();
 			for (Principale principal : principales) {
 				principalesOrdenados.add(principal);
 			}
-			ModelAndView salida=new ModelAndView("principale");
+			ModelAndView salida = new ModelAndView("principal");
 			salida.addObject("principales", principalesOrdenados);
 			return salida;
-			
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			return new ModelAndView("error") ;
+			return new ModelAndView("error");
 		}
-		
-		
-		
-		
+
 	}
 
-
-
 	@RequestMapping("modificaPrincipal.html")
-	public ModelAndView modificaPrincipal(int id)
-	{
-		Principale principal=null;
-		if(id==0)
-		{
-			principal=new Principale();
+	public ModelAndView modificaPrincipal(int id) {
+		Principale principal = null;
+		if (id == 0) {
+			principal = new Principale();
 		}
-		
-		
+
 		try {
-			if(id>0)
-			 principal=(Principale) getPrincipalDelegate().dameObjeto(id, Principale.class);			
-			ModelAndView salida=new ModelAndView("modificaPrincipal");
-			salida.addObject("principal",principal);
+			if (id > 0)
+				principal = (Principale) getPrincipalDelegate().dameObjeto(id,
+						Principale.class);
+			ModelAndView salida = new ModelAndView("modificaPrincipal");
+			salida.addObject("principal", principal);
 			return salida;
-			 
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -85,54 +69,26 @@ private static final Logger logger = LoggerFactory.getLogger(PrincipalController
 		}
 	}
 
-
-	
 	@RequestMapping("grabaPrincipal.html")
-	public ModelAndView grabaPrincipal(Principale principal)
-	{
-		
-		
+	public ModelAndView grabaPrincipal(Principale principal) {
+
 		try {
 			getPrincipalDelegate().grabaObjeto(principal);
 			return principales();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		
-		return new ModelAndView("error");
+
+			return new ModelAndView("error");
 		}
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 	public PrincipalDelegate getPrincipalDelegate() {
 		return principalDelegate;
 	}
-
-
-
-
 
 	public void setPrincipalDelegate(PrincipalDelegate principalDelegate) {
 		this.principalDelegate = principalDelegate;
 	}
 
-
-
-
-
-
-	
-	
-	
-	
-	
-	
 }
