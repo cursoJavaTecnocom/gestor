d877a0ecedd9684773006d74fe42ff89f74f5698
profesor <profesor@londres1.formacion.local>
2015-06-23 12:15:51 +0200

    Menu

--- src/main/java/curso/tecnocom/gestor/delegates/MenuDelegate.java
+++ src/main/java/curso/tecnocom/gestor/delegates/MenuDelegate.java
@@ -3,11 +3,13 @@ package curso.tecnocom.gestor.delegates;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import curso.tecnocom.gestor.datos.Menu;
 import curso.tecnocom.gestor.datos.TipoMenu;
 import curso.tecnocom.gestor.services.MenuService;
 
+@Component
 public class MenuDelegate {
 
 	@Autowired
