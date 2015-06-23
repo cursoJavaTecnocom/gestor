c3b82961eb4a823e0f24dec8413a597245b54ba7
alumno <alumno@londres8.formacion.local>
2015-06-23 17:12:10 +0200

    contenido

--- src/main/java/curso/tecnocom/gestor/ContenidoController.java
+++ src/main/java/curso/tecnocom/gestor/ContenidoController.java
@@ -5,12 +5,14 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import curso.tecnocom.gestor.datos.Contenido;
 import curso.tecnocom.gestor.delegates.ContenidoDelegate;
 
+@Controller
 public class ContenidoController {
 
 	private static final Logger logger = LoggerFactory
