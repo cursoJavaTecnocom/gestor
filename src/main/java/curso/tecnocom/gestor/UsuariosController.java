6aadd42dc7c6b29d70bed3eae015b588d050763f
alumno <alumno@londres14.formacion.local>
2015-06-23 12:59:54 +0200

    usuario

--- src/main/java/curso/tecnocom/gestor/UsuariosController.java
+++ src/main/java/curso/tecnocom/gestor/UsuariosController.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import tecnocom.curso.empresa.HomeController;
+
 import curso.tecnocom.gestor.datos.Usuario;
 
 
