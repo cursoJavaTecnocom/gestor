07d7148a137e158f39807a1621e76755f5cd2838
alumno <alumno@londres15.formacion.local>
2015-06-23 13:27:22 +0200

    Nuevos .js Destacados

---
+++ src/main/webapp/js/destacado.js
@@ -0,0 +1,20 @@
+
+
+function borraDestacado(id)
+{
+	
+	document.location="borraDestacado.gin?id="+id;
+
+}
+
+function modificaEmpresa(id)
+{
+
+	document.location="modificaDestacado.gin?id="+id;
+
+}
+
+
+
+
+
