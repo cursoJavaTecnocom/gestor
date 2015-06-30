d8042101e31b6aefcdb5e83c443c1fa3d46b3510
Javier Diaz <jdiaz@inelde.com>
2015-06-30 15:48:48 +0200

    dao

--- src/main/java/curso/tecnocom/gestor/daos/GestorDao.java
+++ src/main/java/curso/tecnocom/gestor/daos/GestorDao.java
@@ -4,17 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.hibernate.Criteria;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 import curso.tecnocom.gestor.anotaciones.SessionManager;
 import curso.tecnocom.gestor.anotaciones.TransactionManager;
 import curso.tecnocom.gestor.datos.Principale;
 
-@Repository
-public class GestorDao implements DaoInterface {
+@Component
+public class GestorDao  {
 
 	private Session sesion;
 
@@ -37,59 +39,45 @@ public class GestorDao implements DaoInterface {
 		this.factoria = factoria;
 	}
 
-	@Override
-	//@SessionManager
+	//@Override
+	@SessionManager
 	public List<?> dameDatos(Class<?> clase) throws Exception {
-		setSesion(getFactoria().openSession());
-		Criteria criteria = getSesion().createCriteria(clase);
-		List<?> lista=criteria.list();
-		List<Principale> lista1=  new ArrayList<Principale>();
-		for (Object object : lista) {
-			lista1.add((Principale) object);
-			
-		}
-		getSesion().close();
-		return lista1;
+		
+		Query q= getSesion().createQuery("from "+clase.getName()+" e");
+		//Criteria criteria = getSesion().createCriteria(clase);
+		List<?> lista=q.list();
+		
+		
+		return lista;
 	}
 
-	@Override
-	//@SessionManager
+//	@Override
+	@SessionManager
 	public Object dameObjeto(int id, Class<?> clase) throws Exception {
-		setSesion(getFactoria().openSession());
 		return getSesion().load(clase, id);
 
 	}
 
-	@Override
-	//@TransactionManager
+	//@Override
+	@TransactionManager
 	public void grabaDato(Object objeto) throws Exception {
-		setSesion(getFactoria().openSession());
-		getSesion().beginTransaction();
 		getSesion().saveOrUpdate(objeto);
-		getSesion().getTransaction().commit();
+
 	}
 
-	@Override
-	//@TransactionManager
-	public void borraDato(Object objeto) throws Exception
-	{
+	//@Override
+	@TransactionManager
+	public void borraDato(Object objeto) throws Exception {
 		// TODO Auto-generated method stub
-		
-		setSesion(getFactoria().openSession());
-		getSesion().beginTransaction();
 		getSesion().delete(objeto);
-		getSesion().getTransaction().commit();
 	}
 
-	@Override
-	//@TransactionManager
+	//@Override
+	@TransactionManager
 	public void borraDato(int id, Class<?> clase) throws Exception {
 		// TODO Auto-generated method stub
-		setSesion(getFactoria().openSession());
-		getSesion().beginTransaction();
 		Object objeto = dameObjeto(id, clase);
 		getSesion().delete(objeto);
-		getSesion().getTransaction().commit();
 	}
 
 }
