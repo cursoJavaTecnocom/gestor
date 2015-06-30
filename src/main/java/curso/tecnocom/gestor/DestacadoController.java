b973296e81fbcaca88f9085f7511f063d3381b7f
alumno <alumno@londres17.formacion.local>
2015-06-30 12:15:39 +0200

    carga

--- src/main/java/curso/tecnocom/gestor/DestacadoController.java
+++ src/main/java/curso/tecnocom/gestor/DestacadoController.java
@@ -1,7 +1,6 @@
 package curso.tecnocom.gestor;
 
 import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,9 +8,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-
 import curso.tecnocom.gestor.datos.Destacado;
-import curso.tecnocom.gestor.delegates.DestacadosDelegate;
+import curso.tecnocom.gestor.delegates.GestorDelegate;
+
 @Controller
 public class DestacadoController {
 
@@ -21,7 +20,7 @@ public class DestacadoController {
 			.getLogger(DestacadoController.class);
 
 	@Autowired
-	private DestacadosDelegate destacadosDelegate;
+	private GestorDelegate gestorDelegate;
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home() {
@@ -32,8 +31,7 @@ public class DestacadoController {
 	@RequestMapping("destacados.gin")
 	public ModelAndView destacados() {
 		try {
-			List<Destacado> destacados = (List<Destacado>) getDestacadosDelegate()
-					.recuperaDatos(Destacado.class);
+			List<Destacado> destacados = (List<Destacado>) getGestorDelegate().dameDatos(Destacado.class);
 			ModelAndView salida = new ModelAndView("destacados");
 			salida.addObject("destacados", destacados);
 			return salida;
@@ -47,7 +45,7 @@ public class DestacadoController {
 	@RequestMapping("borraDestacado.gin")
 	public ModelAndView borraDestacado(int id) {
 		try {
-			getDestacadosDelegate().borraObjeto(id, Destacado.class);
+			getGestorDelegate().borraDato(id, Destacado.class);
 			return destacados();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
@@ -64,8 +62,7 @@ public class DestacadoController {
 		}
 		try {
 			if (id > 0) {
-				destacado = (Destacado) getDestacadosDelegate().recuperaObjeto(
-						id, Destacado.class);
+				destacado = (Destacado) getGestorDelegate().dameObjeto(id, Destacado.class);
 			}
 			ModelAndView salida = new ModelAndView("modificaDestacado");
 			salida.addObject("destacado", destacado);
@@ -77,13 +74,29 @@ public class DestacadoController {
 		}
 
 	}
-
-	public DestacadosDelegate getDestacadosDelegate() {
-		return destacadosDelegate;
+	
+	
+	@RequestMapping("grabaDestacado.html")
+	public ModelAndView grabaEmpresa(Destacado destacado, String titulo, String texto)
+	{
+		try {
+			getGestorDelegate().grabaObjeto(destacado);
+			return destacados();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			destacado.setTitulo(titulo);
+			destacado.setTexto(texto);
+			e.printStackTrace();
+			return new ModelAndView("error");
+		}
 	}
 
-	public void setDestacadoDelegate(DestacadosDelegate destacadosDelegate) {
-		this.destacadosDelegate = destacadosDelegate;
+	public GestorDelegate getGestorDelegate() {
+		return gestorDelegate;
+	}
+
+	public void setGestorDelegate(GestorDelegate gestorDelegate) {
+		this.gestorDelegate = gestorDelegate;
 	}
 
 }
