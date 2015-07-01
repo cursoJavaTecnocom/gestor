d109dc1e7a11bf17377044279baa2cf6b2396996
alumno <alumno@londres11.formacion.local>
2015-07-01 16:23:29 +0200

    contenido

--- src/main/java/curso/tecnocom/gestor/ContenidoController.java
+++ src/main/java/curso/tecnocom/gestor/ContenidoController.java
@@ -1,5 +1,6 @@
 package curso.tecnocom.gestor;
 
+
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -8,10 +9,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import curso.tecnocom.gestor.datos.Contenido;
+import curso.tecnocom.gestor.datos.ContenidoProperty;
+import curso.tecnocom.gestor.datos.Imagene;
+import curso.tecnocom.gestor.datos.ImagenesProperty;
+import curso.tecnocom.gestor.datos.Menu;
+import curso.tecnocom.gestor.datos.MenuProperty;
+import curso.tecnocom.gestor.datos.TipoMenu;
+import curso.tecnocom.gestor.datos.TipoMenuProperty;
 import curso.tecnocom.gestor.delegates.GestorDelegate;
 
 @Controller
@@ -22,11 +33,18 @@ public class ContenidoController {
 
 	@Autowired
 	private GestorDelegate contenidoDelegate;
+	
+	@InitBinder
+	public void init(WebDataBinder binder){
+
+		binder.registerCustomEditor(Contenido.class, new ContenidoProperty());
+	}
 
 	@SuppressWarnings("unchecked")
 	@RequestMapping("contenidos.html")
 	public ModelAndView contenido(HttpServletRequest request) {
-		 
+		//if (!getContenidoDelegate().validar(request))
+			//return new ModelAndView("noLogado");
 
 		try {
 			List<Contenido> contenidos = (List<Contenido>) getContenidoDelegate()
@@ -46,40 +64,56 @@ public class ContenidoController {
 
 	@RequestMapping("modificaContenido.html")
 	public ModelAndView modificaContenido(int id,HttpServletRequest request) {
-		if (getContenidoDelegate().validar(request))
-			return new ModelAndView("noLogado");
+		//if (getContenidoDelegate().validar(request))
+			//return new ModelAndView("noLogado");
 		try {
-			if (id == 0) {
-				Contenido contenido = new Contenido();
-			} else {
-				Contenido contenido = (Contenido) getContenidoDelegate()
-						.dameDatos(Contenido.class);
-			}
+			Contenido contenido=null;
+			if (id == 0) 
+				contenido = new Contenido();
+			 else 
+				contenido = (Contenido) getContenidoDelegate().dameObjeto(id, Contenido.class);
+			
+			ModelAndView salida = new ModelAndView("modificaContenido");
+			salida.addObject("contenido", contenido);
+			return salida;
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			return new ModelAndView ("error");
 		}
 
-		ModelAndView salida = new ModelAndView("modificaContenido");
-		return salida;
+		
 
 	}
 
 	@RequestMapping("grabaContenido.html")
-	public ModelAndView grabaContenido(Contenido contenido,HttpServletRequest request) {
-		if (getContenidoDelegate().validar(request))
-			return new ModelAndView("noLogado");
+	public ModelAndView grabaContenido(Contenido contenido,HttpServletRequest request, BindingResult result) {
+		//if (getContenidoDelegate().validar(request))
+			//return new ModelAndView("noLogado");
 		try {
 			getContenidoDelegate().grabaObjeto(contenido);
 			return contenido(request);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-
 			return new ModelAndView("error");
 		}
 	}
-
+	
+	@RequestMapping("borraContenido.html")
+	public ModelAndView borraContenido(int id, HttpServletRequest request){
+		try {
+			getContenidoDelegate().borraDato(id, Contenido.class);
+			return contenido(request);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+		
+	}
+	
+	
 	public GestorDelegate getContenidoDelegate() {
 		return contenidoDelegate;
 	}
@@ -88,3 +122,4 @@ public class ContenidoController {
 		this.contenidoDelegate = contenidoDelegate;
 	}
 }
+
