877eda4ff11f13588ba4a2bb12a3b8c911f21247
alumno <alumno@londres4.formacion.local>
2015-07-01 09:16:14 +0200

    menu

--- src/main/java/curso/tecnocom/gestor/services/GestorService.java
+++ src/main/java/curso/tecnocom/gestor/services/GestorService.java
@@ -6,7 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import curso.tecnocom.gestor.daos.DaoInterface;
+import curso.tecnocom.gestor.daos.GestorDao;
 
 @Service
 public class GestorService {
