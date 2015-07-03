a21e9345dd1d5b35cbb478c34ccbdd67944078a3
alumno <alumno@londres9.formacion.local>
2015-07-03 12:56:45 +0200

    js modifica usuarios

--- src/main/webapp/js/usuariosjs.js
+++ src/main/webapp/js/usuariosjs.js
@@ -9,3 +9,25 @@ function modificaUsuario(id)
 {
 document.location="modificaUsuario.html?id="+id	
 }
+
+function validateForm() {
+    var x = document.forms["miFormulario"]["usuario"].value;
+    var y = document.forms["miFormulario"]["clave"].value;
+    if (x == null || x == "" || y == null || y == "") 
+    {
+       if(   y == null || y == "" ) {
+    	   alert("La clave no puede estar vacia");
+    	   document.getElementById("cla").style.background = "#FF6C47";
+    	   document.getElementById("cla").focus();
+    	   
+       }
+       else {
+    	   alert("El usuario no puede estar vacio");
+    	   document.getElementById("usu").style.background = "#FF6C47";
+    	   document.getElementById("usu").focus();
+       }
+   
+       
+        return false;
+    }
+}
